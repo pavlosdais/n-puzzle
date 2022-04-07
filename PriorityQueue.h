ec378766e1d11b3638e003344ec6e5f05d91c48b
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-04-08 01:09:14 +0300

    Delete PriorityQueue.h

--- PriorityQueue.h
+++
@@ -1,8 +0,0 @@
-#include "item.h"
-typedef struct PriorityQueueNode* PriorityQueue;
-
-void QueueInit(PriorityQueue* Queue);
-char IsEmpty(PriorityQueue Queue);
-void Enqueue(PriorityQueue* Queue, puzzlePointer puzzle, int g, int h);
-puzzlePointer Dequeue(PriorityQueue* Queue, int* g, int* h);
-void DestroyPQueue(PriorityQueue Queue, int n);
